a4fff99349a1a3cd4cebd1492be1597d7d8607b1
Mathias Köppchen - Protronic GmbH <m.koeppchen@protronic-gmbh.de>
2024-08-21 07:26:50 +0200

    add float and basic math to rhai example

--- examples/stm32wba/src/bin/usart_rhai.rs
+++ examples/stm32wba/src/bin/usart_rhai.rs
@@ -15,7 +15,7 @@ use embassy_stm32::{
 };
 use {defmt_rtt as _, panic_probe as _};
 
-use rhai::{packages::Package, Dynamic, Engine};
+use rhai::{packages::Package, packages::BasicMathPackage, Dynamic, Engine};
 
 bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
@@ -31,7 +31,7 @@ async fn main(_spawner: Spawner) {
     // Initialize the allocator BEFORE you use it
     {
         use core::mem::MaybeUninit;
-        const HEAP_SIZE: usize = 64 * 1024;
+        const HEAP_SIZE: usize = 92 * 1024;
         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
         unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
     }
@@ -45,6 +45,10 @@ async fn main(_spawner: Spawner) {
     let mut usart = Uart::new_blocking(p.USART1, p.PA8, p.PB12, config1).unwrap();
 
     let mut engine = Engine::new_raw();
+    let package = BasicMathPackage::new();
+
+    // Register the package into the 'Engine'.
+    package.register_into_engine(&mut engine);
 
     // this bit gets commented out to test size without Core
     let std = rhai::packages::CorePackage::new();
