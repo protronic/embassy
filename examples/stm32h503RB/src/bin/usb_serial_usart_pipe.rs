c4998331c5478ca5473bdefa4e9423c996d3cbf3
Mathias Köppchen - Protronic GmbH <m.koeppchen@protronic-gmbh.de>
2025-01-23 20:47:30 +0100

    work usb serial uart pipe

--- examples/stm32h503RB/src/bin/usb_serial_usart_pipe.rs
+++ examples/stm32h503RB/src/bin/usb_serial_usart_pipe.rs
@@ -3,15 +3,16 @@
 
 use defmt::{panic, *};
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
+use embassy_futures::join::{join, join3};
 use embassy_stm32::time::Hertz;
 use embassy_stm32::usb::{Driver, Instance};
 use embassy_stm32::{bind_interrupts, peripherals, usb, usart, Config};
 use embassy_stm32::mode::Async;
-use embassy_stm32::usart::{Uart, UartRx};
-use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_stm32::usart::{Uart, UartRx, UartTx};
+use embassy_sync::blocking_mutex::raw::{NoopRawMutex, ThreadModeRawMutex};
 use embassy_sync::channel::Channel;
-use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_sync::pipe::Pipe;
+use embassy_usb::class::cdc_acm::{CdcAcmClass, Receiver, Sender, State};
 use embassy_usb::driver::EndpointError;
 use embassy_usb::Builder;
 use {defmt_rtt as _, panic_probe as _};
@@ -20,10 +21,6 @@ bind_interrupts!(struct Irqs {
     USB_DRD_FS => usb::InterruptHandler<peripherals::USB>;
     USART3 => usart::InterruptHandler<peripherals::USART3>;
 });
-
-static RX_CHANNEL: Channel<ThreadModeRawMutex, [u8; 64], 1> = Channel::new();
-static TX_CHANNEL: Channel<ThreadModeRawMutex, [u8; 64], 1> = Channel::new();
-
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -58,9 +55,7 @@ async fn main(_spawner: Spawner) {
 
     let config = embassy_stm32::usart::Config::default();
     let mut usart = Uart::new(p.USART3, p.PA3, p.PA4, Irqs, p.GPDMA1_CH0, p.GPDMA1_CH1, config).unwrap();
-    let (mut tx, rx) = usart.split();
-
-    unwrap!(_spawner.spawn(reader(rx)));
+    let (mut uart_tx, uart_rx) = usart.split();
 
     // Create the driver, from the HAL.
     let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
@@ -97,42 +92,41 @@ async fn main(_spawner: Spawner) {
     // Run the USB device.
     let usb_fut = usb.run();
 
-    // Do stuff with the class!
-    let echo_fut = async {
+    // Pipe setup
+    let mut usb_pipe: Pipe<NoopRawMutex, 20> = Pipe::new();
+    let (mut usb_pipe_reader, mut usb_pipe_writer) = usb_pipe.split();
+
+    let mut uart_pipe: Pipe<NoopRawMutex, 20> = Pipe::new();
+    let (mut uart_pipe_reader, mut uart_pipe_writer) = uart_pipe.split();
+
+    let (mut usb_tx, mut usb_rx) = class.split();
+
+    // Read + write from USB
+    let usb_future = async {
         loop {
-            class.wait_connection().await;
+            info!("Wait for USB connection");
+            usb_rx.wait_connection().await;
             info!("Connected");
-            let _ = echo(&mut class).await;
+            let _ = join(
+                usb_read(&mut usb_rx, &mut uart_pipe_writer),
+                usb_write(&mut usb_tx, &mut usb_pipe_reader),
+            )
+                .await;
             info!("Disconnected");
         }
     };
 
+    // Read + write from UART
+    let uart_future = join(
+        uart_read(uart_rx, &mut usb_pipe_writer),
+        uart_write(uart_tx, &mut uart_pipe_reader),
+    );
+
     // Run everything concurrently.
     // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, echo_fut).await;
-
-    loop {
-        let tx_buf = TX_CHANNEL.receive().await;
-        info!("writing...");
-        unwrap!(tx.write(&tx_buf).await);
-        let rx_buf = RX_CHANNEL.receive().await;
-        info!("writing...");
-        unwrap!(class.write_packet(&rx_buf).await);
-    }
-
-
+    join3(usb_fut, usb_future, uart_future).await;
 }
 
-#[embassy_executor::task]
-async fn reader(mut rx: UartRx<'static, Async>) {
-    let mut buf = [0; 64];
-    loop {
-        info!("reading...");
-        unwrap!(rx.read_until_idle(&mut buf).await);
-        info!("data: {:x}", buf);
-        RX_CHANNEL.send(buf).await;
-    }
-}
 
 struct Disconnected {}
 
@@ -145,14 +139,64 @@ impl From<EndpointError> for Disconnected {
     }
 }
 
-async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+/// Read from the USB and write it to the UART TX pipe
+async fn usb_read<'d, T: Instance + 'd>(
+    usb_rx: &mut Receiver<'d, Driver<'d, T>>,
+    uart_pipe_writer: &mut embassy_sync::pipe::Writer<'_, NoopRawMutex, 20>,
+) -> Result<(), Disconnected> {
     let mut buf = [0; 64];
     loop {
-        let n = class.read_packet(&mut buf).await?;
+        let n = usb_rx.read_packet(&mut buf).await?;
         let data = &buf[..n];
-        info!("data: {:x}", data);
-        TX_CHANNEL.send(buf);
+        trace!("USB IN: {:x}", data);
+        (*uart_pipe_writer).write(data).await;
     }
 }
+
+/// Read from the USB TX pipe and write it to the USB
+async fn usb_write<'d, T: Instance + 'd>(
+    usb_tx: &mut Sender<'d, Driver<'d, T>>,
+    usb_pipe_reader: &mut embassy_sync::pipe::Reader<'_, NoopRawMutex, 20>,
+) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = (*usb_pipe_reader).read(&mut buf).await;
+        let data = &buf[..n];
+        trace!("USB OUT: {:x}", data);
+        usb_tx.write_packet(&data).await?;
+    }
+}
+
+/// Read from the UART and write it to the USB TX pipe
+async fn uart_read(
+    mut uart_rx: UartRx<'static, Async>,
+    usb_pipe_writer: &mut embassy_sync::pipe::Writer<'_, NoopRawMutex, 20>,
+) -> ! {
+    let mut buf = [0; 64];
+    loop {
+        let n = uart_rx.read_until_idle(&mut buf).await.expect("UART read error");
+        if n == 0 {
+            continue;
+        }
+        let data = &buf[..n];
+        trace!("UART IN: {:x}", buf);
+        (*usb_pipe_writer).write(data).await;
+    }
+}
+
+/// Read from the UART TX pipe and write it to the UART
+async fn uart_write(
+    mut uart_tx: UartTx<'static, Async>,
+    uart_pipe_reader: &mut embassy_sync::pipe::Reader<'_, NoopRawMutex, 20>,
+) -> ! {
+    let mut buf = [0; 64];
+    loop {
+        let n = (*uart_pipe_reader).read(&mut buf).await;
+        let data = &buf[..n];
+        trace!("UART OUT: {:x}", data);
+        let _ = uart_tx.write(&data).await;
+    }
+}
+
 
 
